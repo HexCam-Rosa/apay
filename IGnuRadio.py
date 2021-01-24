aef88233d975ca214babbd0930ac5724281bd671
Daniel <dan_larby@hotmail.co.uk>
2021-01-24 16:31:05 +0000

    working p2p fks

--- IGnuRadio.py
+++ IGnuRadio.py
@@ -13,7 +13,7 @@ socket = context.socket(zmq.PUB)
 socket.bind("tcp://127.0.0.1:%s" % port)
 
 #while True:
-for i in range(5):
+for i in range(100):
     #topic = np.random.random_integers(0,1, 100).astype(np.int8)
     #topic = np.random.random_integers(-128,127, 100).astype(np.int8)
     #topic = np.random.random_integers(5,5, 100).astype(np.int8)
@@ -23,9 +23,13 @@ for i in range(5):
     #string_rep = "Testsentence"
     #print(string_rep)
     #byte_data = np.array([127,5,0,8]).astype(np.ubyte)
-    a = [170,170,170,255,0,255,0,]
+    # a = [170,170,170,255,0,255,0,]
+    # a = np.random.choice([0, 1], 100)
+    # a = np.ones(100)
+    a = np.array([1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0])
     #a = [3,1,0,2,3,2,1]
     socket.send(pmt.serialize_str(pmt.to_pmt(a)))
     #print(pmt.to_pmt(a))
     #time.sleep(0.5)
+    print(a)
     time.sleep(0.05)
